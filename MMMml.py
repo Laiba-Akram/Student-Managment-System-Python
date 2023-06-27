bc50fb0b99cba62dca7b74a37869c468c07a52ae
Laiba-Akram <developerlaiba067@gmail.com>
2023-06-27 08:37:14 +0500

    first commit

---
+++ MMMml.py
@@ -0,0 +1,16 @@
+import numpy
+
+speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+x = numpy.mean(speed)
+
+print("The Speed of mean is :  " + str(x))
+
+
+
+
+distance = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+y = numpy.median(distance)
+
+print("The median of distance is :  " + str(y))
---
+++ dapnd.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv("C:\\Users\\Perfect computer\\Downloads\\AIMLDL Course\\archive\\iris.csv")
+print(df)
